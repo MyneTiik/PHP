63676c33369b64b1d33373e189ab1a59c5248f72
CPTHOMBO <165996476+CPTHOMBO@users.noreply.github.com>
2024-04-04 10:11:52 +0200

    Update bdd.sql
    ajout équipes

--- bdd.sql
+++ bdd.sql
@@ -1,9 +1,9 @@
 #DROP TABLE maillot_dom;
 
 CREATE TABLE maillot_dom
-  (idequipe    VARCHAR(15)    NOT NULL,
-   idmaillotdom    VARCHAR(15)    NOT NULL,
-   nomequipe    VARCHAR(15)    NOT NULL,
+  (idequipe    VARCHAR(3)    NOT NULL,
+   idmaillotdom    VARCHAR(3)    NOT NULL,
+   nomequipe    VARCHAR(20)    NOT NULL,
    marque    VARCHAR(15)    NULL,
    CONSTRAINT cle_comp PRIMARY KEY (refcomp)
   );
@@ -11,15 +11,16 @@ CREATE TABLE maillot_dom
 INSERT INTO maillots VALUES('1','1','Liverpool','Nike');
 INSERT INTO maillots VALUES('2','2','Manchester City','Puma');
 INSERT INTO maillots VALUES('3','3','Arsenal','Adidas');
-
+INSERT INTO maillots VALUES('4','4','Manchester United','Adidas');
+INSERT INTO maillots VALUES('5','5','Lutton','Umbro');
 
 
 #DROP TABLE maillot_exte;
 
 CREATE TABLE maillot_exte
-  (idequipe    VARCHAR(15)    NOT NULL,
-   idmaillotexte    VARCHAR(15)    NOT NULL,
-   nomequipe    VARCHAR(15)    NOT NULL,
+  (idequipe    VARCHAR(3)    NOT NULL,
+   idmaillotexte    VARCHAR(3)    NOT NULL,
+   nomequipe    VARCHAR(20)    NOT NULL,
    marque    VARCHAR(15)    NULL,
    CONSTRAINT cle_comp PRIMARY KEY (idequipe)
   );
@@ -27,3 +28,5 @@ CREATE TABLE maillot_exte
 INSERT INTO maillots VALUES('1','1','Liverpool','Nike');
 INSERT INTO maillots VALUES('2','2','Manchester City','Puma');
 INSERT INTO maillots VALUES('3','3','Arsenal','Adidas');
+INSERT INTO maillots VALUES('4','4','Manchester United','Adidas');
+INSERT INTO maillots VALUES('5','5','Lutton','Umbro');
