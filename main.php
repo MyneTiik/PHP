15a28ef109b457bd337aee6553d3674bfc9bc74f
MyneTiik <156836756+MyneTiik@users.noreply.github.com>
2024-04-09 08:45:21 +0200

    Update main.php
    add lines and modif header

--- main.php
+++ main.php
@@ -2,13 +2,16 @@ class  page_accueil {
     function page_accueil($titre)
         { $this->titre ; }
     function debut()
-        {echo "<html lang="fr"><head>$this->titre <meta charset="UTF-8"></head><body>"} 
+        {echo "<html><head>$this->titre" ;} 
+    function entete()
+        { echo "<meta charset='UTF-8'></head><body>" ; }
     function fin()
-        {echo "</body></html>"}
+        {echo "</body></html>" ;}
 
 }
 
 $s = new page_accueil("Footix.com") ;
 $s->debut() ;
+$s->entete() ;
 echo "<p>Sur le serveur, il est exactement ".date("H:i:s")."</p>" ;
 $s->fin() ;
