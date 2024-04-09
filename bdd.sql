58de3194244f17b7ebd9a1aca0463d1a7a4e1929
CPTHOMBO <165996476+CPTHOMBO@users.noreply.github.com>
2024-04-09 10:51:11 +0200

    Update bdd.sql
    ajout image peut etre

--- bdd.sql
+++ bdd.sql
@@ -7,14 +7,16 @@ CREATE TABLE maillot_dom
    idmaillotdom    INTEGER    NOT NULL,
    nomequipe    TEXT    NOT NULL,
    marque    TEXT    NOT NULL,
-   prix    INTEGER    NOT NULL
-   );
+   prix    INTEGER    NOT NULL,
+   im_avant    TEXT    NOT NULL,
+   im_dos    TEXT    NOT NULL 
+  );
 
-INSERT INTO maillot_dom VALUES(1,1,'Liverpool','Nike',9999);
-INSERT INTO maillot_dom VALUES(2,2,'Manchester City','Puma',90);
-INSERT INTO maillot_dom VALUES(3,3,'Arsenal','Adidas',85);
-INSERT INTO maillot_dom VALUES(4,4,'Manchester United','Adidas',75);
-INSERT INTO maillot_dom VALUES(5,5,'Luton','Umbro',90);
+INSERT INTO maillot_dom VALUES(1,1,'Liverpool','Nike',9999,'ressource_image/liverpool_dom_avant.jpeg','ressource_image/liverpool_dom_avant.jpeg');
+INSERT INTO maillot_dom VALUES(2,2,'Manchester City','Puma',90,'ressource_image/mancity_dom_avant.jpeg','ressource_image/mancity_dom_avant.jpeg');
+INSERT INTO maillot_dom VALUES(3,3,'Arsenal','Adidas',85,'ressource_image/arsenal_dom_avant.jpeg','ressource_image/arsenal_dom_avant.jpeg');
+INSERT INTO maillot_dom VALUES(4,4,'Manchester United','Adidas',75,'ressource_image/manunited_dom_avant.jpeg','ressource_image/manunited_dom_avant.jpeg');
+INSERT INTO maillot_dom VALUES(5,5,'Luton','Umbro',90,'ressource_image/luton_dom_avant.jpeg','ressource_image/luton_dom_avant.jpeg');
 
 
 DROP TABLE IF EXISTS maillot_exte;
@@ -24,14 +26,16 @@ CREATE TABLE maillot_exte
    idmaillotexte    INTEGER    NOT NULL,
    nomequipe    TEXT    NOT NULL,
    marque    TEXT    NOT NULL,
-   prix    INTEGER    NOT NULL
+   prix    INTEGER    NOT NULL,
+   im_avant    TEXT    NOT NULL,
+   im_dos    TEXT    NOT NULL 
    );
 
-INSERT INTO maillot_exte VALUES(1,1,'Liverpool','Nike',9999);
-INSERT INTO maillot_exte VALUES(2,2,'Manchester City','Puma',90);
-INSERT INTO maillot_exte VALUES(3,3,'Arsenal','Adidas',85);
-INSERT INTO maillot_exte VALUES(4,4,'Manchester United','Adidas',75);
-INSERT INTO maillot_exte VALUES(5,5,'Luton','Umbro',90);
+INSERT INTO maillot_exte VALUES(1,1,'Liverpool','Nike',9999,'ressource_image/liverpool_exte_avant.jpeg','ressource_image/liverpool_exte_avant.jpeg');
+INSERT INTO maillot_exte VALUES(2,2,'Manchester City','Puma',90,'ressource_image/mancity_exte_avant.jpeg','ressource_image/mancity_exte_avant.jpeg');
+INSERT INTO maillot_exte VALUES(3,3,'Arsenal','Adidas',85,'ressource_image/arsenal_exte_avant.jpeg','ressource_image/arsenal_exte_avant.jpeg');
+INSERT INTO maillot_exte VALUES(4,4,'Manchester United','Adidas',75,'ressource_image/manunited_exte_avant.jpeg','ressource_image/manunited_exte_avant.jpeg');
+INSERT INTO maillot_exte VALUES(5,5,'Luton','Umbro',90,'ressource_image/luton_exte_avant.jpeg','ressource_image/luton_exte_avant.jpeg');
 
 
 
