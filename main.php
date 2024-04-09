a618a134fbb30304ee44bfa13edf074beccf3080
MyneTiik <156836756+MyneTiik@users.noreply.github.com>
2024-04-09 09:15:03 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,12 +1,17 @@
 class  page_accueil {
+	public $titre = "no";
+	
     function page_accueil($titre)
         { $this->titre ; }
     function debut()
-        {echo "<html><head>$this->titre" ;} 
+        {echo "<html><head><title>$this->titre</title>" ;} 
     function entete()
         { echo "<meta charset='UTF-8'></head><body>" ; }
+    function corps()
+        {echo "<h1> LE FOOT </h1>" ; }
     function fin()
         {echo "</body></html>" ;}
+    
 
 }
 
@@ -14,4 +19,5 @@ $s = new page_accueil("Footix.com") ;
 $s->debut() ;
 $s->entete() ;
 echo "<p>Sur le serveur, il est exactement ".date("H:i:s")."</p>" ;
+$s->corps();
 $s->fin() ;
