4663414a47ed16e8f95c5618808c3362349be170
MyneTiik <156836756+MyneTiik@users.noreply.github.com>
2024-05-14 11:37:05 +0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Page</title>
+</head>
+<body>
+    <h1>Page admin</h1>
+
+
+    <h2>Ajouter des valeurs</h2>
+
+    <form action="adminpage.php" method="POST">
+        <label for="nomequipe">Nom de l'équipe</label>
+        <input type="text" name="nomequipe" id="nomequipe">
+        <label for="marque">Marque</label>
+        <input type="text" name="marque" id="marque">
+        <label for="prix">Prix</label>
+        <input type="text" name="prix" id="prix">
+        <label for="maillot">Maillot</label>
+        <input type="text" name="maillot" id="maillot">
+        <input type="submit" value="Ajouter">
+        
+</body>
+</html>
