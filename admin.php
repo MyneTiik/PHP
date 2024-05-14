a38cd3989c99419a60a6c61fe33fe8a9377535e0
CPTHOMBO <165996476+CPTHOMBO@users.noreply.github.com>
2024-05-14 14:36:18 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html>
 <head>
     <title>Admin Page</title>
@@ -9,16 +9,12 @@
 
     <h2>Ajouter des valeurs</h2>
 
-    <form action="adminpage.php" method="POST">
-        <label for="nomequipe">Nom de l'équipe</label>
-        <input type="text" name="nomequipe" id="nomequipe">
-        <label for="marque">Marque</label>
-        <input type="text" name="marque" id="marque">
-        <label for="prix">Prix</label>
-        <input type="text" name="prix" id="prix">
-        <label for="maillot">Maillot</label>
-        <input type="text" name="maillot" id="maillot">
-        <input type="submit" value="Ajouter">
-        
-</body>
-</html>
+    <?php
+    $db = new SQLite3('basefoot.sqlite');
+    $results = $db->query('SELECT * FROM membres');
+    while ($row = $results->fetchArray()) {
+    	echo "<option value=\"{$row['pseudo']}\">{$row['pseudo']}</option>";
+    }
+    ?>  
+ 
+ 	
