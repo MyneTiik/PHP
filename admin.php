d2b624c4ad219fdd6ac3bed91d0fb7eef7dadb3a
CPTHOMBO <165996476+CPTHOMBO@users.noreply.github.com>
2024-05-16 09:34:47 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -2,6 +2,11 @@
 <html>
 <head>
     <title>Admin Page</title>
+    <link rel='stylesheet' href='css/style.css'>
+    <title>Footix.com</title>
+    <meta charset='UTF-8'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
     <h1>Page admin</h1>
@@ -22,7 +27,7 @@
     $maillot_exte = $_GET["maillot_exte"];
     $membres = $_GET["membres"];
     ?> 
-    <form action="index.php" method="GET" class="mb-2">
+    <form action="admin.php" method="GET" class="mb-3">
         <div class="input-group">
             <select name="name" class="form-select">
                 <?php
@@ -31,9 +36,19 @@
                     echo "<option value=\"{$row['name']}\">{$row['name']}</option>";
                 }
                 ?>
+                
             </select>
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </div>
+        <?php
+        $db = new SQLite3('basefoot.sqlite');
+        $tables = $db->query('select name from sqlite_master where type="table";');
+        
+        echo "Sous tableau";
+        foreach($tables as $table) {
+        	echo $table['name'] . "<br>";
+        }
+        ?>
     </form>
 	
 
