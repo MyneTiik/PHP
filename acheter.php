83836c3487d11103a7afffce8343bfd9154c4fdb
CPTHOMBO <165996476+CPTHOMBO@users.noreply.github.com>
2024-05-21 21:46:01 +0200

    Add files via upload

---
+++ acheter.php
@@ -0,0 +1,13 @@
+<?php include 'head.php'; ?>
+<?php include 'nav.php'; ?>
+
+<div class="container">
+    <h1>Merci pour votre commande !</h1>
+    <p>Nous vous remercions pour votre achat. Vous recevrez votre votre commande d'ici 15 jours.</p>
+    <p>N'h&eacutesitez pas &agrave; laisser un commentaire !<p>
+    <a href="commentaire.php" class="btn btn-primary">Laissez un commentaire</a>
+    <p>Si vous avez des questions, n'h&eacutesitez pas &agrave; nous contacter.</p>
+    <a href="index.php" class="btn btn-primary">Retour &agrave; l'accueil</a>
+</div>
+
+<?php include 'footer.php'; ?>
